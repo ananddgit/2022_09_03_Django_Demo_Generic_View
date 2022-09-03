4639e23bddc7013e6e189d9543e0b02a4dc0c8d2
ajmalrashadmarkz <ajmalrashadmarkz@gmail.com>
2022-09-03 13:04:28 +0530

    Django Generic View - Demo

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\2022\09_Sept\01_Django\09_22_Demo_Generic_View\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
